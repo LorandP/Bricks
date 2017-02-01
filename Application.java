2a12f5e9f376e1f52ee07c6a79f0d73562d3654b
Lori <palfalvi.lorand@gmail.com>
2017-02-01 19:15:22 +0200

    Final version.

--- Application.java
+++ Application.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 /**
  * Created by Lori on 2/1/2017.
@@ -7,57 +8,70 @@ import java.util.Arrays;
 public class Application {
 
     /**
-     * This method tries to build a goal inches long row full of bricks.
-     * Returns true if it is possible to make the goal by choosing from the given bricks.
-     *
-     * @param smallBrick 1 inch long bricks
-     * @param bigBrick   5 inch long bricks
-     * @param goal       the desired dimension of the row     *
+     * This method invokes two other methods that try to fill up the goal inch long row lenght and
+     * returns true if the the row could be built by small and big bricks that it is provided with.
+     * @param smallBrick number of small bricks
+     * @param bigBrick number of big bricks
+     * @param row_length the length of the row
+     * @return
      */
-    private String makeBricks(int smallBrick, int bigBrick, int goal) {
+    private String makeBricks(int smallBrick, int bigBrick, int row_length) {
         String True = String.valueOf(true);
         String False = String.valueOf(false);
-        int s_Brick = 1;
-        int b_Brick = 5;
-        int numberOf_s_Bricks = 1;
-        int numberOf_b_Bricks = 1;
-        int totalBricks = 0;
-        int row_lengh = 0;
-       // int[] row_lenght;
-
-        if (goal > 0) {
-            //row_lenght = new int[goal];
-            if (smallBrick < 0 || bigBrick < 0)
-                return "Only positive values allowed for smallBrick and bigBrick.";
-            numberOf_s_Bricks = smallBrick * s_Brick;
-            numberOf_b_Bricks = bigBrick * b_Brick;
-            totalBricks = numberOf_b_Bricks + numberOf_s_Bricks;
-            if (totalBricks < goal)
-                return "There are not enough bricks.";
-            System.out.println("Total bricks = " + totalBricks);
-            row_lengh = goal - totalBricks;
-            if (row_lengh < 0) {
-                String numberAsString = Integer.toString(row_lengh);
-                return "Reminder = " + numberAsString;
-            }
-              //row_lenght = addBricks(row_lenght,totalBricks);
-
-        } else
-            return "Please enter a valid length for the goal.";
-
-        //  System.out.format("Big bricks = %d Small bricks %d",numberOf_b_Bricks, numberOf_s_Bricks);
-        return True;
+        int goalFromBigBricks = bigBricksFirst(bigBrick, row_length);
+        boolean goalFromSmallBricks;
+        if (goalFromBigBricks == 0)
+            return True;
+        else {
+            goalFromSmallBricks = smallBricksNext(smallBrick, goalFromBigBricks);
+            if (goalFromSmallBricks == true)
+                return True;
+        }
+        return False;
     }
 
-    private static int[] addBricks(int[] row, int total_bricks){
-        row = Arrays.copyOf(row, row.length + 1);
-        row[row.length - 1] = total_bricks;
-        return row;
+    /**
+     * This method takes the row length and subtracts one big brig from it in recursive way.
+     * If it succeeded returns true if not then returns the remaining row length.
+     * @param bigBrick number of big bricks
+     * @param row_length the length of the row
+     * @return just whatever integer, it will not reach that anyway
+     */
+        private int bigBricksFirst(int bigBrick, int row_length) {
+        int b_Brick = 5;
+        String numberAsString = Integer.toString(row_length);
+        String True = String.valueOf(true);
+
+        if (bigBrick == 0 && row_length > 0)
+            return row_length;
+        if (row_length == 0)
+            return row_length;
+        if (row_length - b_Brick >= 0) {
+            row_length -= b_Brick;
+            return bigBricksFirst(bigBrick - 1, row_length);
+        }
+        return 1;
+    }
+
+    /**
+     * This method takes the remaining row length after it was subtracted from by all the big bricks first.
+     * Returns true if there are enough small bricks to fill the remaining space in the row.
+     * Returns false if there aren't any small bricks left to fill in the remaining space in the row.
+     * @param smallBrick the number of small bricks
+     * @param new_row_length the remaining row length
+     * @return true and false.
+     */
+    private boolean smallBricksNext(int smallBrick, int new_row_length)
+    {
+        if (smallBrick >= new_row_length)
+            return true;
+        else
+            return false;
     }
 
     public static void main(String[] args) {
         Application application = new Application();
 
-        System.out.println(application.makeBricks(0,3, 13));
+        System.out.println(application.makeBricks(3, 1, 8));
     }
 }
